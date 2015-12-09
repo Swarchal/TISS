8072d565571ae262c7b29164bc397f794d35a878
Swarchal <S.Warchal@sms.ed.ac.uk>
2015-12-09 17:44:47 +0000

    check inputs are dataframe or matrix

---
+++ R/ks_cols.R
@@ -0,0 +1,28 @@
+#' Signed KS test across pairs of columns
+#' 
+#' Given two numerical columns of matrices, this function will compute a signed
+#' D-statistic for every pair of columns.
+#' 
+#' @param f dataframe or matrix of sample
+#' @param g dataframe or matrix of control
+#' 
+#' @return tmp vector of D-statistics
+#' 
+#' @export
+
+ks_cols <- function(f, g){
+    
+    # check inputs
+    if (!is.matrix(f) && !is.data.frame(f)){
+        stop("f needs to be a dataframe of a matrix")
+    }
+    if (!is.matrix(g) && !is.data.frame(g)){
+        stop("g needs to be a dataframe of a matrix")
+    }
+    
+    tmp <- vector(length = ncol(g))
+    for (i in 1:ncol(df_r)){
+        tmp[i] <- ks_test(f[, i], g[, i])
+    }
+    return(tmp)
+}
