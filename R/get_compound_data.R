34177f7397b86c695a523d7724819c742d2a3c56
Swarchal <S.Warchal@sms.ed.ac.uk>
2015-12-10 14:56:22 +0000

    lapply -> sapply, keeps data as dataframes

--- R/get_compound_data.R
+++ R/get_compound_data.R
@@ -14,18 +14,18 @@
 
 get_compound_data <- function(df, metadata){
     
-    if(!is.data.frame(df)) stop("df has to be a dataframe")
-    if(!is.list(metadata)) stop("metadata has to be a list")
+    if (!is.data.frame(df)) stop("df has to be a dataframe")
+    if (!is.list(metadata)) stop("metadata has to be a list")
     
     # construct list of list of dataframes
     
     # list of compounds
     split_by_compound <- split(df, metadata$compounds)
     # each element 'compound' contains list of dataframes for each concentration
-    split_all <- lapply(split_by_compound, split, f = metadata$concentrations)
+    split_all <- sapply(split_by_compound, split, f = metadata$concentrations)
     
     # subset featuredata
-    split_featuredata <- lapply(split_all, '[', metadata$feature_cols)
+    split_featuredata <- sapply(split_all, '[', metadata$feature_cols)
     
     return(split_featuredata)
 }
