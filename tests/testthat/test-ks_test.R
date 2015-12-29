80c6d9375d4e2d8d2ebd5ff2f09a0cac514b9b87
Swarchal <S.Warchal@sms.ed.ac.uk>
2015-12-29 23:47:54 +0000

    further tests for ks_test

--- tests/testthat/test-ks_test.R
+++ tests/testthat/test-ks_test.R
@@ -1,9 +1,25 @@
 context("Check signed KS test works")
 
-test_that("signed ks_test",{
-    set.seed(12321) # for reproducibility
-    a <- rnorm(100, 1, 10)
-    b <- rnorm(100, 1, 1)
-    out <- ks_test(a, b)
+set.seed(12321) # for reproducibility
+a <- rnorm(100, 1, 10)
+b <- rnorm(100, 1, 1)
+out <- ks_test(a, b)
+
+test_that("signed ks_test returns a number",{
+    expect_is(out, "numeric")
+})
+
+test_that("signed ks_test returns same number as base ks.test",{
+    base_out <- ks.test(a, b)[[1]]
+    expect_equivalent(base_out, abs(out))
+})
+
+test_that("signed ks_test returns expected number",{
     expect_equal(out, -0.53)
+})
+
+out_same <- ks_test(a, a)
+
+test_that("ks_test returns 0 for same distribution",{
+    expect_equal(out_same, 0)
 })
