88cc0b822ab99031907ee136e5e126e592e2e9d8
Swarchal <S.Warchal@sms.ed.ac.uk>
2016-01-18 17:39:22 +0000

    fix, now takes strings or indices as arguments

--- R/construct_metadata.R
+++ R/construct_metadata.R
@@ -3,10 +3,10 @@
 #' Creates a list containing all necessary metadata to subset a dataframe
 #' 
 #' @param df Dataframe
-#' @param compound_col Vector or dataframe column
-#' @param conc_col Vector or dataframe column
-#' @param feature_cols Vector
-#' @param negative_control string
+#' @param compound_col index or name of dataframe column
+#' @param conc_col index or name of dataframe column
+#' @param feature_cols Vector of indices that match numeric feature columns
+#' @param negative_control string, name of negative control in compound_col
 #' 
 #' @return metadata list containing all metadata
 #' 
@@ -17,15 +17,13 @@ construct_metadata <- function(df, compound_col, conc_col, feature_cols, negativ
     
     # check input
     if(!is.data.frame(df)) stop("df needs to be a dataframe")
-    if(!is.vector(compound_col)) stop("compound_col needs to be a vector of compound names")
-    if(!is.vector(conc_col)) stop("conc_col needs to be a vector of numeric concentration values")
     if(!is.numeric(feature_cols)) stop("feature_cols needs to be a numeric vector")
     if(!is.character(negative_control)) stop("negative_control needs to be a character string")
     
     # remove negative control from compound list
-    all_compounds <- unique(df$compound_col)
+    all_compounds <- unique(df[, compound_col])
     compounds <- all_compounds[! all_compounds %in% negative_control]
-    concentrations <- unique(df$conc_col)
+    concentrations <- unique(df[, conc_col])
     
     # construct list of metadata
     metadata <- list(compounds = compounds,
