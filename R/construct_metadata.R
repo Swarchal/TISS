83e2bf93ceacb62823e3e30fb40421911b7993a2
Swarchal <S.Warchal@sms.ed.ac.uk>
2016-01-20 14:55:31 +0000

    add concentration col to metadata

--- R/construct_metadata.R
+++ R/construct_metadata.R
@@ -38,7 +38,8 @@ construct_metadata <- function(df, compound_col, conc_col, feature_cols, negativ
                      concentrations = concentrations,
                      feature_cols = feature_cols,
                      negative_control = negative_control,
-                     compound_col = compound_col)
+                     compound_col = compound_col,
+                     concentration_col = conc_col)
 
     class(metadata) <- "metadata"
     
