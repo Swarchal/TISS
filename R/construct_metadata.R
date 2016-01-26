de3af3942c1d873ed90bfc1f717624c5817399f2
Swarchal <S.Warchal@sms.ed.ac.uk>
2016-01-26 13:06:00 +0000

    add example

--- R/construct_metadata.R
+++ R/construct_metadata.R
@@ -1,16 +1,37 @@
 #' Constructs metadata list to be used in functions
 #' 
-#' Creates a list containing all necessary metadata to subset a dataframe
+#' Creates a list containing all necessary metadata to subset a dataframe.
+#' Used within other functions as a useful container and stops repeated
+#' input of dataframe information.
 #' 
 #' @param df Dataframe
-#' @param compound_col index or name of dataframe column
-#' @param conc_col index or name of dataframe column
-#' @param feature_cols Vector of indices that match numeric feature columns
-#' @param negative_control string, name of negative control in compound_col
+#' @param compound_col index or name of dataframe column for compound names
+#' @param conc_col index or name of dataframe column for concentration data
+#' @param feature_cols Vector of indices that match numeric feature columns.
+#'  a feature column is one that contains numerical information of a cellular
+#'  measurement such as area, or cell_count. Metadata such as image_number or
+#'  object_number_ID is not feature data.
+#' @param negative_control string, name of negative control in compound_col.
+#' e.g \code{'DMSO'}
 #' 
-#' @return metadata list containing all metadata
+#' @return metadata List containing all metadata.
 #' 
 #' @export
+#'
+#' @examples
+#' # construct example dataframe
+#' concentration <- rep(c(100, 10, 1, 0.1), 10)
+#' compounds <- sort(rep(letters[1:10], 4))
+#' cell_area <- rnorm(40)
+#' no_speckles <- rnorm(40, 100, 100)
+#' df <- data.frame(compounds, concentration, cell_area, speckles)
+#'
+#' construct_metadata(
+#'     df,
+#'     compound_col = "compounds",
+#'     conc_col = "concentration",
+#'     feature_cols = 3:4,
+#'     negative_control = "a")
 
 
 construct_metadata <- function(df, compound_col, conc_col, feature_cols, negative_control){
