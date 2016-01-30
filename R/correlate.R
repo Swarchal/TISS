f444db40105f4694f355aa91e7cb93d18a15d421
Swarchal <S.Warchal@sms.ed.ac.uk>
2016-01-30 19:11:19 +0000

    add correlate function

---
+++ R/correlate.R
@@ -0,0 +1,93 @@
+#' Internal function used in correlate()
+#'
+#' \code{trim_cor} should be used interactively.
+#'
+#' @param x First cor_vector
+#' @param y Second vector
+#' @param n Number of shifts in either direction
+#' @param p Number of features per shift
+#' @return cor_measurements
+
+
+# bare-bones trim_cor function
+trim_cor_ <- function(x, y, n, p){
+	
+	cor_measurements <- vector(length = 2 * n + 1)
+	
+	# left shift from -n to -1
+	for (i in n:1){
+	    x_tmp <- tail(x, - i * p)
+	    y_tmp <- head(y, - i * p)
+	    cor_measurements[i] <- cor(x_tmp, y_tmp)
+	}
+	# reverse measurements so that they are in the correct order
+	# going from -n -> 0 -> +n
+	cor_measurements[1:n] <- rev(cor_measurements[1:n])
+	
+	# cor of unshifted x and y
+	cor_measurements[n + 1] <- cor(x, y)
+	
+	# right shift from 1 to n
+	for (i in 1:n){
+	    x_tmp <- head(x, - i * p)
+	    y_tmp <- tail(y, - i * p)
+	    cor_measurements[n + 1 + i] <- cor(x_tmp, y_tmp)
+	}
+	
+	# name of shift in vector of correlation values
+	names(cor_measurements)[1:n] <- paste("-", n:1)
+	names(cor_measurements)[n + 1] <- 0
+	names(cor_measurements)[(n + 2):length(cor_measurements)] <- paste('+', 1:n)
+
+	# create class for plot_trim_cor
+	class(cor_measurements) <- "cor_vector"
+	
+	return(cor_measurements)
+	
+}
+
+
+#' Calculates correlation between vectors shifted by titrations.
+#'
+#' For a matrix, with each row representing a compound vector constructed of
+#' features and titrations. \code{correlate} will calculate the correlation 
+#' for each vector compared against the average of all the other vectors,
+#' shifted by a number of titrations in both directions. This is to align
+#' compound vectors invariant of differing potencies.
+#'
+#' @param x Matrix or dataframe of compound vectors. Each row representing
+#'    an individual compound vector
+#' @param metadata corresponding metadata for x, produced by
+#'   \code{construct_metadata}
+#' @return List of correlation measurements, each element of the list
+#'   corresponds to a compound vector in the order given by \code{x}
+#'
+#' @export
+
+correlate <- function(x, metadata){
+
+	if (class(metadata) != 'metadata'){
+		stop("Metadata has to be a metadata object calculated by 'construct_metadata()'")
+	}
+
+	n <- as.integer(length(metadata$concentrations) / 2)
+    p <- length(metadata$feature_cols)
+
+	# 2. initialise somewhere to store optimal shifts for each row
+	out_list <- vector('list', length = nrow(x))
+
+	# 3. main loop for each row in x
+	for (row in 1:nrow(x)){
+
+		tmp <- as.numeric(x[row, ])
+		rest <- x[-row, ]
+		av_rest <- as.numeric(colMeans(rest))
+		out_list[[row]] <- trim_cor_(
+			x = tmp,
+			y = av_rest,
+			n = n,
+			p = p)
+	}
+
+	return(out_list)
+}
