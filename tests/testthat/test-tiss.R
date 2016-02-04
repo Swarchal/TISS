778a42a30eaf3796f38fd3af65b0a16e0f5f780d
Swarchal <S.Warchal@sms.ed.ac.uk>
2016-02-04 17:15:49 +0000

    fix failing test

--- tests/testthat/test-tiss.R
+++ tests/testthat/test-tiss.R
@@ -8,7 +8,7 @@ metadata <- construct_metadata(ex_data,
                                feature_cols = 2:68,
                                negative_control = "DMSO")
 
-tiss_out <- tiss(data = exdata, metadata = metadata)
+tiss_out <- tiss(data = ex_data, metadata = metadata)
 tiss_unaligned <- tiss(data = ex_data, metadata = metadata, align = FALSE)
 
 test_that("tiss works as expected",{
