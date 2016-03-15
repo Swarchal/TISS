be186e8384a8e9e529da4bce06740b3ece72b753
Swarchal <S.Warchal@sms.ed.ac.uk>
2016-03-15 15:18:22 +0000

    add data docs and description ref

---
+++ R/ex_data.R
@@ -0,0 +1,9 @@
+#' example data
+#'
+#' example data from a typical CellProfiler screen
+#'
+#' @format dataframe containing column per feature, row per observation
+#'
+#' @source Scott Warchal
+
+"ex_data"
